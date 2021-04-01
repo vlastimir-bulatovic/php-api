df8ca790cf822c417a8bd022911c386d414e5c39
vlastimir-bulatovic <bulatovic.vlastimir98@gmail.com>
2021-04-01 23:25:16 +0200

    updated

---
+++ database/migration/user.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `user` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` text NOT NULL ,
+    `email` text NOT NULL,
+    `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
